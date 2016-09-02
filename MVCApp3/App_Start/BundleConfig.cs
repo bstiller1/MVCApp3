4856be4f9a6a032efdf860c5a2912989fbedd781
Blake Stiller <stiller.blake@gmail.com>
2016-09-02 16:28:47 -0400

    MVC C# 2016
    Code from 09-02-2016
    MVC App Example

---
+++ MVCApp3/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Optimization;
+
+
+namespace MVCApp3.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.validate"));
+            bundles.Add(new ScriptBundle("~/Content/css").Include(
+                "~/Content/boostrap.css", "~/Content/site.css"));
+        }
+    }
+}
